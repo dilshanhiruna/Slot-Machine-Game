93bc13abff02b895f2bdf895f2dc00899fdadc70
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-14 18:50:55 +0530

    created Login ui & implemented login/register functions

--- app/src/main/java/User/Login.java
+++ app/src/main/java/User/Login.java
@@ -1,16 +1,74 @@
 package User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.oop.slot_machine_game.MainActivity;
 import com.oop.slot_machine_game.R;
 
+
+
 public class Login extends AppCompatActivity {
+
+    EditText password, email;
+    Button signin;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email= findViewById(R.id.emailLoginput);
+        password= findViewById(R.id.passwordLoginput);
+        signin= findViewById(R.id.signinbtn);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String memail = email.getText().toString().trim();
+                String mpassword = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(memail)){
+                    email.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(mpassword)){
+                    password.setError("Password is required");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(memail,mpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(Login.this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/User/Register.java
+++ app/src/main/java/User/Register.java
@@ -1,16 +1,74 @@
 package User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.oop.slot_machine_game.R;
 
 public class Register extends AppCompatActivity {
+
+    EditText password, email;
+    Button signup,signininstead;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email= findViewById(R.id.emailReginput);
+        password= findViewById(R.id.passwordReginput);
+        signup= findViewById(R.id.signupbtn);
+        signininstead= findViewById(R.id.signininstead);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        signininstead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String memail = email.getText().toString().trim();
+                String mpassword = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(memail)){
+                    email.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(mpassword)){
+                    password.setError("Password is required");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(memail,mpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull  Task<AuthResult> task) {
+                        Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+
+
+
     }
 }
