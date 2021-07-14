c475aeede72eb4e83052461a2b65cd243520c096
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-14 17:19:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/oop/slot_machine_game/MainActivity.java
@@ -0,0 +1,14 @@
+package com.oop.slot_machine_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
